58290131f18362cf639a2ee2275b12b7d4bb4e31
CKPRASHANTH07 <ckprashanthc@gmail.com>
2024-04-24 14:55:59 +0530

    modified:   src/pages/LandingPages/First4.js 	modified:   src/pages/LandingPages/Last4.js 	new file:   src/pages/images/Designer.png 	new file:   src/pages/images/artist.png 	new file:   src/pages/images/join.png

--- src/pages/LandingPages/First4.js
+++ src/pages/LandingPages/First4.js
@@ -1,108 +1,144 @@
 import React from 'react'
+import bgart from '../images/Designer.png'
+import artist from '../images/artist.png'
 
 const First4 = () => {
   return (
     <div>
-        <div className=' bg-gray-400 p-32 space-y-8 space-x-4'>
-      <h1>
-        Welcome to Art Vistas
-</h1>
-<p>
-Unlock a world of global art opportunities and connect with visionary artists from around the world. Explore, showcase, and collaborate on the international art stage.
-</p>
-<div className='space-x-4'>
-  <button className='primary-button'>Join</button>
-  <button className='secondary-button'>Learn More</button>
-</div>
+    <div className='p-8 md:p-32 space-y-8 space-x-4 relative'>
+  <img src={bgart} alt='' className='absolute inset-0 w-full h-full object-cover z-0' />
+  <div className='relative z-10 backdrop-blur-md space-y-10 bg-white/30 rounded-2xl p-10'>
+    <h1>Welcome to Art Vistas</h1>
+    <p >Unlock a world of global art opportunities and connect with visionary artists from around the world. Explore, showcase, and collaborate on the international art stage.</p>
+    <div className='space-x-4'>
+      <button className='primary-button'>Join</button>
+      <button className='secondary-button'>Learn More</button>
     </div>
+  </div>
+</div>
 
 
-    
-<ol className="relative border-s border-gray-200 dark:border-gray-700 p-20">                  
-    <li className="mb-10 ms-4">
-        <div className="absolute w-20 h-3 bg-gray-200 mt-1.5 start-1.5 dark:bg-gray-700">
-            1
-        </div>
-        <h2>February 2022</h2>
-        <p>Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
-         </li>
-         <li className="mb-10 ms-4">
-        <div className="absolute w-20 h-3 bg-gray-200 mt-1.5 -start-1.5 dark:bg-gray-700">
-            2
-        </div>
-        <h2>February 2022</h2>
-        <p>Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
-         </li>
-         <li className="mb-10 ms-4">
-        <div className="absolute w-20 h-3 bg-gray-200 mt-1.5 -start-1.5 dark:bg-gray-700">
-            3
-        </div>
-        <h2>February 2022</h2>
-        <p>Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
-         </li>
-</ol>
 
 
+<div className='relative bg-slate-500 p-20'>
+  <div>
+    <h1 className='mb-20 text-white'>Discover Global Art Stage Openings</h1>
+    <div className="grid grid-cols-2 gap-6 px-32">
+      <div className="p-6 bg-gray-100 rounded-lg">
+        <h2 className="text-xl font-semibold mb-4">Explore</h2>
+        <p className="text-gray-700">
+          Dive into our curated database of international art exhibitions, residencies, and festivals, spanning a wide range of media and genres.
+        </p>
+      </div>
+      <div className="p-6 bg-gray-100 rounded-lg">
+        <h2 className="text-xl font-semibold mb-4">Apply</h2>
+        <p className="text-gray-700">
+          Easily submit your applications to the opportunities that align with your artistic vision and career goals. Our user-friendly platform streamlines the process.
+        </p>
+      </div>
+      <div className="p-6 bg-gray-100 rounded-lg">
+        <h2 className="text-xl font-semibold mb-4">Connect</h2>
+        <p className="text-gray-700">
+          Forge meaningful connections with art professionals and organizations around the world. Unlock valuable networking opportunities and unlock new collaborations.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 
    
-<div className='bg-gray-300 py-24 '>
-<h1>Empowering Artists from Extinction Countries</h1>
-<div className="grid border border-gray-200 rounded-lg shadow-sm  md:mb-12 md:grid-cols-2 bg-white m-20 ">
-    <figure className="flex flex-col items-center justify-center p-8 text-center border-2">
-        <blockquote className="max-w-2xl mx-auto mb-4 lg:mb-8">
-            <h2>Visibility</h2>
-            <p>We shine a spotlight on the extraordinary talents of artists from regions facing cultural, political, or economic challenges. Our platform amplifies their voices and connects them to a global audience.</p>
+<div className='relative bg-transparent py-24'>
+  <div className='relative z-10 py-24'>
+    <h1 className='mb-6 backdrop-blur-md bg-white/50 w-fit  md:text-4xl text-xl rounded-2xl'>Empowering Artists from Extinction Countries</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mx-4 md:mx-20">
+      <figure className="flex flex-col items-center justify-center p-8 text-center bg-white/50 backdrop-blur-md  rounded-lg shadow-md">
+        <blockquote className="max-w-md mx-auto mb-4 lg:mb-8">
+          <h2 className="text-xl font-semibold mb-4">Visibility</h2>
+          <p>We shine a spotlight on the extraordinary talents of artists from regions facing cultural, political, or economic challenges. Our platform amplifies their voices and connects them to a global audience.</p>
         </blockquote>
-          
-    </figure>
-    <figure className="flex flex-col items-center justify-center p-8 text-center border-2">
-        <blockquote className="max-w-2xl mx-auto mb-4 lg:mb-8">
-            <h2>Mentorship</h2>
-            <p>Through our artist-to-artist mentorship program, experienced professionals guide and support emerging artists, sharing invaluable insights and fostering their creative growth.</p>
+      </figure>
+      <figure className="flex flex-col items-center justify-center p-8 text-center bg-white/50 backdrop-blur-md rounded-lg shadow-md">
+        <blockquote className="max-w-md mx-auto mb-4 lg:mb-8">
+          <h2 className="text-xl font-semibold mb-4">Mentorship</h2>
+          <p>Through our artist-to-artist mentorship program, experienced professionals guide and support emerging artists, sharing invaluable insights and fostering their creative growth.</p>
         </blockquote>
-          
-    </figure><figure className="flex flex-col items-center justify-center p-8 text-center border-2">
-        <blockquote className="max-w-2xl mx-auto mb-4 lg:mb-8">
-            <h2>Community</h2>
-            <p>Our community of visionary artists from diverse backgrounds provides a nurturing environment for collaboration, idea-sharing, and mutual inspiration. Together, we break down barriers and redefine the global art landscape.</p>
+      </figure>
+      <figure className="flex flex-col items-center justify-center p-8 text-center bg-white/50 backdrop-blur-md rounded-lg shadow-md">
+        <blockquote className="max-w-md mx-auto mb-4 lg:mb-8">
+          <h2 className="text-xl font-semibold mb-4">Community</h2>
+          <p>Our community of visionary artists from diverse backgrounds provides a nurturing environment for collaboration, idea-sharing, and mutual inspiration. Together, we break down barriers and redefine the global art landscape.</p>
         </blockquote>
-          
-    </figure><figure className="flex flex-col items-center justify-center p-8 text-center border-2 ">
-        <blockquote className="max-w-2xl mx-auto mb-4 lg:mb-8">
-            <h2>Resources</h2>
-            <p>We offer a comprehensive suite of resources, from funding opportunities to professional development workshops, empowering artists to thrive and reach new heights in their creative journeys.</p>
+      </figure>
+      <figure className="flex flex-col items-center justify-center p-8 text-center bg-white/50 backdrop-blur-md rounded-lg shadow-md">
+        <blockquote className="max-w-md mx-auto mb-4 lg:mb-8">
+          <h2 className="text-xl font-semibold mb-4">Resources</h2>
+          <p>We offer a comprehensive suite of resources, from funding opportunities to professional development workshops, empowering artists to thrive and reach new heights in their creative journeys.</p>
         </blockquote>
-          
-    </figure>
-</div>
-</div>
-
-
-    <div className=' bg-slate-500 p-20 '>
-      <div>
-        <h1 className='mb-20'>Discover Global Art Stage Openings</h1>
-        <div className="grid grid-cols-2 gap-6 px-32">
-  <div className="p-6 bg-gray-100 rounded-lg">
-    <h2 className="text-xl font-semibold mb-4">Explore</h2>
-    <p className="text-gray-700">
-    Dive into our curated database of international art exhibitions, residencies, and festivals, spanning a wide range of media and genres. </p>
-  </div>
-  <div className="p-6 bg-gray-100 rounded-lg">
-    <h2 className="text-xl font-semibold mb-4">Apply</h2>
-    <p className="text-gray-700">
-    Easily submit your applications to the opportunities that align with your artistic vision and career goals. Our user-friendly platform streamlines the process.</p>
-  </div>
-  <div className="p-6 bg-gray-100 rounded-lg">
-    <h2 className="text-xl font-semibold mb-4">Connect</h2>
-    <p className="text-gray-700">Forge meaningful connections with art professionals and organizations around the world. Unlock valuable networking opportunities and unlock new collaborations.
-      </p>
-  </div>
-</div>
-
-      </div>
+      </figure>
     </div>
+  </div>
+  <img src={artist} alt='' className='absolute inset-0 w-full h-full object-cover z-0' />
+</div>
+
+
+
+
+    
+
+<section className="py-10 sm:py-16 lg:py-24 relative  bg-gradient-to-b from-gray-400 on to-yellow-200 ">
+  <div className=" mx-auto max-w-7xl sm:px-6 lg:px-8 z-10 bg-black/30 rounded-xl p-10">
+  <h1 className='pt-10'>Discover Global Art Stage Openings</h1>
+    <div className="max-w-2xl mx-auto   text-center">
+    </div>
+
+    <ul className="max-w-md mx-auto mt-16 space-y-12">
+      <li className="relative flex items-start">
+        <div className="-ml-0.5 absolute mt-0.5 top-14 left-8 w-px border-l-4 border-dotted border-gray-300 h-full" aria-hidden="true"></div>
+
+        <div className="relative flex items-center justify-center flex-shrink-0 w-16 h-16 bg-white rounded-full shadow">
+          <svg className="w-10 h-10 text-fuchsia-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+          </svg>
+        </div>
+        <div className="ml-6">
+          <h2>Explore</h2>
+          <p>Dive into our curated database of international art exhibitions, residencies, and festivals, spanning a wide range of media and genres.</p>
+        </div>
+      </li>
+
+      <li className="relative flex items-start">
+        <div className="-ml-0.5 absolute mt-0.5 top-14 left-8 w-px border-l-4 border-dotted border-gray-300 h-full" aria-hidden="true"></div>
+
+        <div className="relative flex items-center justify-center flex-shrink-0 w-16 h-16 bg-white rounded-full shadow">
+          <svg className="w-10 h-10 text-fuchsia-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
+          </svg>
+        </div>
+        <div className="ml-6">
+          <h2>Apply</h2>
+          <p>Easily submit your applications to the opportunities that align with your artistic vision and career goals. Our user-friendly platform streamlines the process.</p>
+        </div>
+      </li>
+
+      <li className="relative flex items-start">
+        <div className="relative flex items-center justify-center flex-shrink-0 w-16 h-16 bg-white rounded-full shadow">
+          <svg className="w-10 h-10 text-fuchsia-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+          </svg>
+        </div>
+        <div className="ml-6">
+          <h2>Connect</h2>
+          <p>Forge meaningful connections with art professionals and organizations around the world. Unlock valuable networking opportunities and unlock new collaborations.</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</section>
+
+
+
     </div>
   )
 }
--- src/pages/LandingPages/Last4.js
+++ src/pages/LandingPages/Last4.js
@@ -1,31 +1,35 @@
 import React from 'react'
+import join from '../images/join.png'
+
 
 const Last4 = () => {
   return (
     <div>
-        <div className=' bg-gradient-to-t from-blue-500 to-green-500 p-20 '>
-  <h1>Build Your Artistic Network Worldwide</h1>
-  <div className='flex md:flex-row flex-col items-center p-20'>
-    <div className='w-[400px] m-4'>
+        <div>
+        <div className='py-10 mx-auto sm:py-16 lg:py-24 relative bg-gradient-to-t from-cyan-500 to-yellow-200 '>
+            <div className='bg-black/30 rounded-xl mx-0 xl:mx-20 '>
+  <h1 className='pl-8 pt-20'>Build Your Artistic Network Worldwide</h1>
+  <div className='flex lg:flex-row flex-col items-center space-y-8 p-20'>
+    <div className='w-[400px] '>
       <h2>Global Reach</h2>
       <p>Connect with a diverse community of artists, curators, and art enthusiasts from over 150 countries, expanding your artistic network worldwide.</p>
     </div>
-    <div className='w-[400px] m-4'>
+    <div className='w-[400px]'>
       <h2>Collaboration</h2>
       <p>Engage in meaningful dialogues, share ideas, and co-create innovative projects with like-minded individuals from around the globe.</p>
     </div>
-    <div className='w-[400px] m-4'>
+    <div className='w-[400px]'>
       <h2>Upcoming Events</h2>
       <p>Stay up-to-date on the latest art events, exhibitions, and workshops happening in your local and international communities.</p>
     </div>
-    <div className='w-[400px] m-4'>
+    <div className='w-[400px]'>
       <h2>Showcase Talent</h2>
       <p>Elevate your artistic profile by showcasing your work to a vast audience of art professionals and enthusiasts worldwide.</p>
     </div>
-  </div>
+  </div></div>
     </div>
 
-    <div className='bg-gradient-to-t from-red-300 to-blue-500 p-20 '>
+    <div className='bg-gradient-to-t from-violet-300 to-cyan-500 p-20 '>
       <h1>Showcase Your Work to New Audiences</h1>
       <div className='space-y-10 mt-10'>
       <li>Digital Portfolios</li>
@@ -41,7 +45,7 @@ const Last4 = () => {
 </div>
 </div>
 
-    <div className=' bg-red-300 p-20 '>
+    <div className='  bg-gradient-to-b from-violet-300 to-white p-20 '>
   <h1>Collaborate with Diverse Artistic Communities</h1>
   <div className='flex md:flex-row flex-col items-center p-20'>
     <div className='w-[400px] m-4'>
@@ -57,40 +61,42 @@ const Last4 = () => {
       <p>Expand your artistic horizons, explore new mediums, and push the limits of your creative practice through collaborative endeavors.</p>
     </div>
   </div>
+    </div></div>
+
+
+    <div className='relative bg-transparent py-24'>
+  <div className='relative z-10 py-24'>
+    <h1 className='mb-6 backdrop-blur-md bg-white/50 w-fit md:text-4xl text-xl rounded-2xl'>Join Our Community of Visionary Artists</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mx-4 md:mx-20">
+      <figure className="flex flex-col items-center justify-center p-8 text-center bg-white/50 backdrop-blur-md  rounded-lg shadow-md">
+        <blockquote className="max-w-md mx-auto mb-4 lg:mb-8">
+          <h2 className="text-xl font-semibold mb-4">Global Reach</h2>
+          <p>Tap into a thriving network of artists, curators, and art enthusiasts from over 150 countries, expanding your artistic influence worldwide.</p>
+        </blockquote>
+      </figure>
+      <figure className="flex flex-col items-center justify-center p-8 text-center bg-white/50 backdrop-blur-md rounded-lg shadow-md">
+        <blockquote className="max-w-md mx-auto mb-4 lg:mb-8">
+          <h2 className="text-xl font-semibold mb-4">Unique Opportunities</h2>
+          <p>Discover a curated selection of international art exhibitions, residencies, and funding opportunities tailored to support your creative vision.</p>
+        </blockquote>
+      </figure>
+      <figure className="flex flex-col items-center justify-center p-8 text-center bg-white/50 backdrop-blur-md rounded-lg shadow-md">
+        <blockquote className="max-w-md mx-auto mb-4 lg:mb-8">
+          <h2 className="text-xl font-semibold mb-4">Collaborative Spirit</h2>
+          <p>Engage in cross-cultural dialogues, co-create innovative projects, and form meaningful connections that will inspire and elevate your artistic practice.</p>
+        </blockquote>
+      </figure>
+      <figure className="flex flex-col items-center justify-center p-8 text-center bg-white/50 backdrop-blur-md rounded-lg shadow-md">
+        <blockquote className="max-w-md mx-auto mb-4 lg:mb-8">
+          <h2 className="text-xl font-semibold mb-4">Empowerment</h2>
+          <p>Access a wealth of resources, mentorship programs, and professional development opportunities to help you thrive as an artist and reach new heights in your career.</p>
+        </blockquote>
+      </figure>
     </div>
-
-
-    <div className='bg-gray-300 py-24 '>
-<h1>Join Our Community of Visionary Artists</h1>
-<div className="grid border border-gray-200 rounded-lg shadow-sm  md:mb-12 md:grid-cols-2 bg-white m-20 ">
-    <figure className="flex flex-col items-center justify-center p-8 text-center border-2">
-        <blockquote className="max-w-2xl mx-auto mb-4 lg:mb-8">
-            <h2>Global Reach</h2>
-            <p>Tap into a thriving network of artists, curators, and art enthusiasts from over 150 countries, expanding your artistic influence worldwide.</p>
-        </blockquote>
-          
-    </figure>
-    <figure className="flex flex-col items-center justify-center p-8 text-center border-2">
-        <blockquote className="max-w-2xl mx-auto mb-4 lg:mb-8">
-            <h2>Unique Opportunities</h2>
-            <p>Discover a curated selection of international art exhibitions, residencies, and funding opportunities tailored to support your creative vision.</p>
-        </blockquote>
-          
-    </figure><figure className="flex flex-col items-center justify-center p-8 text-center border-2">
-        <blockquote className="max-w-2xl mx-auto mb-4 lg:mb-8">
-            <h2>Collaborative Spirit</h2>
-            <p>Engage in cross-cultural dialogues, co-create innovative projects, and form meaningful connections that will inspire and elevate your artistic practice.</p>
-        </blockquote>
-          
-    </figure><figure className="flex flex-col items-center justify-center p-8 text-center border-2 ">
-        <blockquote className="max-w-2xl mx-auto mb-4 lg:mb-8">
-            <h2>Empowerment</h2>
-            <p>Access a wealth of resources, mentorship programs, and professional development opportunities to help you thrive as an artist and reach new heights in your career.</p>
-        </blockquote>
-          
-    </figure>
+  </div>
+  <img src={join} alt='' className='absolute inset-0 w-full h-full object-cover z-0' />
 </div>
-</div>
+
     </div>
   )
 }
